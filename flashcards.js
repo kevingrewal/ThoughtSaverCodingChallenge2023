371f183b615e43fdb97430a3a5cbd43fc1fd7943
George Prichard <dgmprichard@gmail.com>
2023-06-02 06:57:24 +0100

    Example Flashcards

---
+++ flashcards.js
@@ -0,0 +1,14 @@
+export const cards = [
+  {
+    front: "What is the French word for 'hello'?",
+    back: "Bonjour",
+  },
+  {
+    front: "What is the French word for 'yes'?",
+    back: "Oui",
+  },
+  {
+    front: "What is the French word for 'no'?",
+    back: "Non",
+  },
+];
---
+++ flashcards.tsx
@@ -0,0 +1,19 @@
+export type Card = {
+  front: string;
+  back: string;
+};
+
+export const cards: Card[] = [
+  {
+    front: "What is the French word for 'hello'?",
+    back: "Bonjour",
+  },
+  {
+    front: "What is the French word for 'yes'?",
+    back: "Oui",
+  },
+  {
+    front: "What is the French word for 'no'?",
+    back: "Non",
+  },
+];
